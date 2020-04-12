18854e2fe5864d71e8821679e954fb4053d49833
Pankaj Kumar Singh <pankaj28843@gmail.com>
2020-04-12 18:05:39 +0530

    fix: add color social icons

--- src/core/config.ts
+++ src/core/config.ts
@@ -17,32 +17,37 @@ export const STATIC_PAGES = [
 ];
 export const SOCIAL_MEDIA = [
   {
+    ariaLabel: "whatsapp",
+    href: "https://api.whatsapp.com/send?phone=919811189189",
+    path: require("../images/whatsapp-icon.svg"),
+  },
+  {
     ariaLabel: "facebook",
-    href: "https://www.facebook.com/mirumeelabs/",
+    href: "https://www.facebook.com/Go4LifeNow",
     path: require("../images/facebook-icon.svg"),
   },
   {
     ariaLabel: "instagram",
-    href: "https://www.instagram.com/mirumeelabs/",
+    href: "https://www.instagram.com/go4lifenow/",
     path: require("../images/instagram-icon.svg"),
   },
-  {
-    ariaLabel: "twitter",
-    href: "https://twitter.com/getsaleor",
-    path: require("../images/twitter-icon.svg"),
-  },
-  {
-    ariaLabel: "youtube",
-    href: "https://www.youtube.com/channel/UCg_ptb-U75e7BprLCGS4s1g/videos",
-    path: require("../images/youtube-icon.svg"),
-  },
+  // {
+  //   ariaLabel: "twitter",
+  //   href: "https://twitter.com/getsaleor",
+  //   path: require("../images/twitter-icon.svg"),
+  // },
+  // {
+  //   ariaLabel: "youtube",
+  //   href: "https://www.youtube.com/channel/UCg_ptb-U75e7BprLCGS4s1g/videos",
+  //   path: require("../images/youtube-icon.svg"),
+  // },
 ];
 export const META_DEFAULTS = {
   custom: [],
   description:
-    "Open-source PWA storefront built with Saleor's e-commerce GraphQL API. Written with React and TypeScript.",
+    "Online store by Go4Life",
   image: `${window.location.origin}${require("../images/logo.png")}`,
-  title: "Demo PWA Storefront – Saleor Commerce",
+  title: "Go4Life",
   type: "website",
   url: window.location.origin,
 };
